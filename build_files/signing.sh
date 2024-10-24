4f59fe4f27f681efcd52681f1117ee6b6eb21527
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-23 19:12:05 -0500

    feat: add signing script (but do not enable)

---
+++ build_files/signing.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+# Signing
+mkdir -p /etc/containers
+mkdir -p /etc/pki/containers
+mkdir -p /etc/containers/registries.d/
+
+if [ -f /usr/etc/containers/policy.json ]; then
+  cp /usr/etc/containers/policy.json /etc/containers/policy.json
+fi
+
+cat <<<"$(jq '.transports.docker |=. + {
+   "ghcr.io/bsherman/bos": [
+    {
+        "type": "sigstoreSigned",
+        "keyPath": "/etc/pki/containers/bos.pub",
+        "signedIdentity": {
+            "type": "matchRepository"
+        }
+    }
+]}' <"/etc/containers/policy.json")" >"/tmp/policy.json"
+cp /tmp/policy.json /etc/containers/policy.json
+cp /ctx/cosign.pub /etc/pki/containers/bos.pub
+tee /etc/containers/registries.d/bos.yaml <<EOF
+docker:
+  ghcr.io/bsherman/bos:
+    use-sigstore-attachments: true
+EOF
+
+mkdir -p /usr/etc/containers/
+cp /etc/containers/policy.json /usr/etc/containers/policy.json
