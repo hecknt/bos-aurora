f351bbdd73ed7fe69d2c1de2a1fa7ed90216da0a
Hec <hec@heccraft.com>
2025-03-06 14:17:33 -0500

    add some packages

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -9,18 +9,33 @@ dnf5 -y copr enable ublue-os/staging
 
 # common packages installed to desktops
 dnf5 install -y \
+  ccache \
+  git \
   gh \
   kitty \
   picard \
-  ibm-plex-fonts-all \
   libpcap-devel \
   libretls \
   ltrace \
   patch \
   pipx \
-  rsms-inter-fonts \
   strace \
   udica \
-  ydotool
+  ydotool \
+  btop \
+  htop \
+  # fonts
+  rsms-inter-fonts \
+  ibm-plex-fonts-all \
+  jetbrains-mono-fonts-all \
+  # virtual machine stuff - install virt-manager through flatpak
+  libvirt \
+  libvirt-daemon-kvm \
+  libvirt-ssh-proxy \
+  libvirt-nss \
+  qemu-img \
+  qemu-kvm \
+  edk2-ovmf \
+  guestfs-tools
 
 dnf5 -y copr disable ublue-os/staging
